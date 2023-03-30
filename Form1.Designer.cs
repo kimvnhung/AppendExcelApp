e70ab0760704f8df4e2a8fcdd972cf8271cbca67
hungkv <kimvnhung@gmail.com>
2023-03-30 22:42:44 +0700

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Append_Excel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.selectedFileList = new System.Windows.Forms.ListView();
+            this.openFileBtn = new System.Windows.Forms.Button();
+            this.appendBtn = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.processStatusLb = new System.Windows.Forms.Label();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Selected Files";
+            // 
+            // selectedFileList
+            // 
+            this.selectedFileList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.selectedFileList.HideSelection = false;
+            this.selectedFileList.Location = new System.Drawing.Point(15, 25);
+            this.selectedFileList.Name = "selectedFileList";
+            this.selectedFileList.Size = new System.Drawing.Size(252, 413);
+            this.selectedFileList.TabIndex = 1;
+            this.selectedFileList.UseCompatibleStateImageBehavior = false;
+            this.selectedFileList.View = System.Windows.Forms.View.Details;
+            // 
+            // openFileBtn
+            // 
+            this.openFileBtn.Location = new System.Drawing.Point(273, 25);
+            this.openFileBtn.Name = "openFileBtn";
+            this.openFileBtn.Size = new System.Drawing.Size(75, 23);
+            this.openFileBtn.TabIndex = 2;
+            this.openFileBtn.Text = "Open Files";
+            this.openFileBtn.UseVisualStyleBackColor = true;
+            this.openFileBtn.Click += new System.EventHandler(this.openFileBtn_Click);
+            // 
+            // appendBtn
+            // 
+            this.appendBtn.Location = new System.Drawing.Point(354, 25);
+            this.appendBtn.Name = "appendBtn";
+            this.appendBtn.Size = new System.Drawing.Size(75, 23);
+            this.appendBtn.TabIndex = 3;
+            this.appendBtn.Text = "Append";
+            this.appendBtn.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(354, 238);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(387, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // processStatusLb
+            // 
+            this.processStatusLb.AutoSize = true;
+            this.processStatusLb.Location = new System.Drawing.Point(354, 219);
+            this.processStatusLb.Name = "processStatusLb";
+            this.processStatusLb.Size = new System.Drawing.Size(37, 13);
+            this.processStatusLb.TabIndex = 5;
+            this.processStatusLb.Text = "Status";
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Width = 256;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.processStatusLb);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.appendBtn);
+            this.Controls.Add(this.openFileBtn);
+            this.Controls.Add(this.selectedFileList);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Append Excel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView selectedFileList;
+        private System.Windows.Forms.Button openFileBtn;
+        private System.Windows.Forms.Button appendBtn;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label processStatusLb;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Append_Excel
+{
+    public partial class Form1 : Form
+    {
+        private string mLastOpenFolder = @"E:\Freelancer\firstjob";
+        private List<string> mSelectedFile = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            selectedFileList.HeaderStyle = ColumnHeaderStyle.None;
+            selectedFileList.View = View.Details;
+        }
+
+        private void openFileBtn_Click(object sender, EventArgs e)
+        {
+            var openFile = new OpenFileDialog
+            {
+                InitialDirectory = mLastOpenFolder,
+                Filter = "Excel Files(*.xls;*.xlsx;*.csv)|*.xls;*.xlsx;*.csv;|All Files(*;)|*;"
+            };
+            openFile.Title = "Open Excel or CSV Files";
+            openFile.Multiselect = true;
+
+            if (openFile.ShowDialog() == DialogResult.OK) {
+                mLastOpenFolder = System.IO.Path.GetDirectoryName(openFile.FileName);
+                mSelectedFile.Clear();
+                selectedFileList.Items.Clear();
+               foreach (String fileName in openFile.FileNames)
+                {
+                    mSelectedFile.Add(fileName);
+                    selectedFileList.Items.Add(System.IO.Path.GetFileName(fileName));
+                }
+            }
+        }
+    }
+}
